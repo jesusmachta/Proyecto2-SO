81316a41ca3f9a56b6652110850394a5fa30166e
Jesus Machta <97565151+jesusmachta@users.noreply.github.com>
2024-10-16 13:01:26 -0400

    :construction:Clases funciones y torneo creadas

---
+++ Proyecto2 SO/src/proyecto2/so/Funciones.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto2.so;
+
+import Estructuras.Queue;
+
+/**
+ *
+ * @author jesusmachta
+ */
+public class Funciones {
+    
+    private Queue cola1Nintendo;
+    private Queue cola2Nintendo;
+    private Queue cola3Nintendo;
+    private Queue refuerzoNintendo;
+    private Queue cola1Capcom;
+    private Queue cola2Capcom;
+    private Queue cola3Capcom;
+    private Queue refuerzoCapcom;
+
+    public Funciones() {
+        this.cola1Nintendo = new Queue();
+        this.cola2Nintendo = new Queue();
+        this.cola3Nintendo = new Queue();
+        this.refuerzoNintendo = new Queue();
+        this.cola1Capcom = new Queue();
+        this.cola2Capcom = new Queue();
+        this.cola3Capcom = new Queue();
+        this.refuerzoCapcom = new Queue();
+        
+    }
+    
+    
+    
+    
+}
---
+++ Proyecto2 SO/src/proyecto2/so/Torneo.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto2.so;
+
+/**
+ *
+ * @author jesusmachta
+ */
+public class Torneo {
+    private Personajes pStarWars;
+    private Personajes pStarTrek;
+    private boolean ganadores;
+    private Personajes ganador;
+
+    public Torneo() {
+    }
+ 
+    public Personajes getpStarWars() {
+        return pStarWars;
+    }
+
+    public void setpStarWars(Personajes pStarWars) {
+        this.pStarWars = pStarWars;
+    }
+
+    public Personajes getpStarTrek() {
+        return pStarTrek;
+    }
+
+    public void setpStarTrek(Personajes pStarTrek) {
+        this.pStarTrek = pStarTrek;
+    }
+    
+    
+    
+    
+}
